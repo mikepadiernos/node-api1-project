2dcf0c5890f276d694a535ad76b0213da1ece41a
Michael Padiernos <mike.padiernos@gmail.com>
2020-03-11 22:11:38 -0400

    [FEATURE] GET / Users & User
    - applied get request for list of users
    - applied get request for user by id

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // implement your API here
+const express = require('express');
+const cors = require('cors');
+const db = require('./data/db');
+
+const server = express();
+
+server.listen(4000, () => {
+	console.log("Server is listening on port 4000");
+});
+
+server.use(express.json());
+server.use(cors());
+
+// HANDLER, GET - "/"
+server.get('/', (request, response) => {
+	response.send('Server... is go');
+});
+
+// HANDLER, GET - "/api/users"
+server.get('/api/users', (request, response) => {
+	db.find()
+		.then(users => {
+			response.status(200).json(users);
+		})
+		.catch(error => {
+			response.status(500).json({success: false, error});
+		})
+});
+
+// HANDLER, GET - "/api/users/:id"
+server.get('/api/users/:id', (request, response) => {
+	db.findById(request.params.id)
+		.then(user => {
+			user ? (
+				response.status(200).json(user)
+			) : (
+				response.status(404).json({success: false, message: 'User not found'})
+			)
+		})
+		.catch(error => {
+			response.status(500).json({success: false, error});
+		})
+});
