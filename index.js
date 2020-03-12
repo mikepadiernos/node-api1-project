8da05538f06c27d421b7abb45365e3cd575349ee
Michael Padiernos <mike.padiernos@gmail.com>
2020-03-11 23:32:01 -0400

    [FEATURE] POST & DELETE / User
    - added ability to add a user
    - added ability to delete a user

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // implement your API here
 const express = require('express');
 const cors = require('cors');
+const id = require('shortid');
 const db = require('./data/db');
 
 const server = express();
@@ -15,6 +16,24 @@ server.use(cors());
 // HANDLER, GET - "/"
 server.get('/', (request, response) => {
 	response.send('Server... is go');
+});
+
+// HANDLER, POST - "/api/users"
+server.post('/api/users', (request, response) => {
+	const user = request.body;
+	console.log("Users Info: ", user);
+
+	db.insert(user)
+		.then(user => {
+			user.name && user.bio ? (
+			response.status(201).json({success: true, user})
+			) : (
+				response.status(400).json({success: false, message: 'Please, provide name & bio.'})
+			)
+		.catch(error => {
+			response.status(500).json({success: false, message: 'User not saved', error})
+		})
+	});
 });
 
 // HANDLER, GET - "/api/users"
@@ -41,4 +60,21 @@ server.get('/api/users/:id', (request, response) => {
 		.catch(error => {
 			response.status(500).json({success: false, error});
 		})
+});
+
+// HANDLER, DELETE - "/api/users/:id"
+server.delete('/api/users/:id', (request, response) => {
+	const { id } = request.params;
+
+	db.remove(id)
+		.then(userDelete => {
+			userDelete ? (
+				response.status(204).end()
+				) : (
+					response.status(404).json({ message: 'User not found' })
+			)
+		})
+		.catch(error=> {
+			response.status(500).json({ success: false, message: 'User can not be removed', error });
+		});
 });
